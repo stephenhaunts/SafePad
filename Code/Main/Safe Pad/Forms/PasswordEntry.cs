78889549ff33b368c073948f64238bda36a3a81f
stephenhaunts_cp <SND\stephenhaunts_cp>
2014-01-30 20:51:56 +0000

    Wire up the password strength class to the password entry form.

--- Code/Main/Safe Pad/Forms/PasswordEntry.cs
+++ Code/Main/Safe Pad/Forms/PasswordEntry.cs
@@ -1,5 +1,6 @@
 ﻿using HauntedHouseSoftware.SecureNotePad.DomainObjects;
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace HauntedHouseSoftware.SecureNotePad.Forms
@@ -54,9 +55,10 @@ namespace HauntedHouseSoftware.SecureNotePad.Forms
             SetPassword(_passwordOne.Text, _passwordTwo.Text);           
         }
 
+        
         private void _passwordOne_KeyPress(object sender, KeyPressEventArgs e)
         {
-            SetPasswordOnReturn(e);
+            SetPasswordOnReturn(e);                      
         }
 
         private void _passwordTwo_KeyPress(object sender, KeyPressEventArgs e)
@@ -71,6 +73,65 @@ namespace HauntedHouseSoftware.SecureNotePad.Forms
                 SetPassword(_passwordOne.Text, _passwordTwo.Text);
                 this.DialogResult = DialogResult.OK;
             }
+        }
+
+        private void PasswordEntry_Load(object sender, EventArgs e)
+        {
+            _passwordOne.Text = "";
+            _passwordTwo.Text = "";
+        }
+
+        private void _passwordOne_TextChanged(object sender, EventArgs e)
+        {
+            ChangePasswordStrengthIndicator(password1Strength, _passwordOne.Text);
+        }
+
+        private void _passwordTwo_TextChanged(object sender, EventArgs e)
+        {
+            ChangePasswordStrengthIndicator(password2Strength, _passwordTwo.Text);
+        }
+
+        private static void ChangePasswordStrengthIndicator(Label label, string password)
+        {
+            PasswordScore score = PasswordStrength.CheckStrength(password);
+            Console.WriteLine(score);
+            switch (score)
+            {
+                case PasswordScore.Blank:
+                    label.ForeColor = Color.Red;
+                    label.Text = "";
+                    break;
+
+                case PasswordScore.Weak:
+                    label.ForeColor = Color.Red;
+                    label.Text = "Weak";
+                    break;
+
+                case PasswordScore.VeryWeak:
+                    label.ForeColor = Color.Red;
+                    label.Text = "Very Weak";
+                    break;
+
+                case PasswordScore.Medium:
+                    label.ForeColor = Color.Orange;
+                    label.Text = "Medium";
+                    break;
+
+                case PasswordScore.Strong:
+                    label.ForeColor = Color.Orange;
+                    label.Text = "Strong";
+                    break;
+
+                case PasswordScore.VeryStrong:
+                    label.ForeColor = Color.Green;
+                    label.Text = "Very Strong";
+                    break;
+            }
+        }
+
+        private void _passwordOne_KeyDown(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
