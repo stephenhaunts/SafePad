52dbbeb4a68c076b40201b285b90a8b561d9fba6
stephenhaunts_cp <SND\stephenhaunts_cp>
2016-01-15 21:22:39 +0000

    Refactored common code from the 1.1 and 1.2 file loader classes into a separate base class.

--- Code/Main/Safe Pad Client Library/DomainObjects/FileFormat/Version12Loader.cs
+++ Code/Main/Safe Pad Client Library/DomainObjects/FileFormat/Version12Loader.cs
@@ -17,82 +17,16 @@
  * 
  * Authors: Stephen Haunts
  */
-using System;
-using HauntedHouseSoftware.SecureNotePad.CryptoProviders;
 
 namespace HauntedHouseSoftware.SecureNotePad.DomainObjects.FileFormat
 {
-    public class Version12Loader : IFileFormatLoader
+    public class Version12Loader : FileLoaderBase, IFileFormatLoader
     {
-        private readonly IAES _aes;
-        private readonly ISecureHash _secureHash;
-        private readonly IPassword _password;
-        private readonly ICompression _compression;
-
-        public Version12Loader(IPassword password)
-        {
-            if (password == null)
-            {
-                throw new ArgumentNullException("password");
-            }
-
-            _aes = new AES();
-            _secureHash = new SecureHash();
-            _password = password;
-            _compression = new GZipCompression();
-        }
+        public Version12Loader(IPassword password) : base(password){ }
 
         public byte[] Load(byte[] byteStream)
         {
-            if (byteStream == null)
-            {
-                throw new ArgumentNullException("byteStream");
-            }
-
-            var versionNumber = ByteHelpers.CreateSpecialByteArray(2);
-            var hash = ByteHelpers.CreateSpecialByteArray(32);
-            var salt = ByteHelpers.CreateSpecialByteArray(32);
-            var encrypted = ByteHelpers.CreateSpecialByteArray((byteStream.Length - 66));
-
-            SplitFileIntoChunks(byteStream, versionNumber, hash, salt, encrypted);
-            CheckFileIntegrity(hash, encrypted);
-
-            return DecryptData(encrypted, salt, 100000);
-        }
-
-        private void CheckFileIntegrity(byte[] hash, byte[] encrypted)
-        {
-            var computedhash = _secureHash.ComputeHash(encrypted);
-
-            if (!ByteHelpers.ByteArrayCompare(computedhash, hash))
-            {
-                throw new InvalidOperationException("The signature of the file does not match.");
-            }
-        }
-
-        private byte[] DecryptData(byte[] encrypted, byte[] salt, int workFactor)
-        {
-            
-            var decrypted = _aes.Decrypt(encrypted, Convert.ToBase64String(_password.CombinedPasswords), salt, workFactor);            
-
-            var decompressed = _compression.Decompress(decrypted);
-
-            return decompressed;
-        }
-
-        private static void SplitFileIntoChunks(byte[] buffer, byte[] versionNumber, byte[] hash, byte[] salt, byte[] encrypted)
-        {
-            int offset = 0;
-            Buffer.BlockCopy(buffer, offset, versionNumber, 0, 2);
-            offset += 2;
-
-            Buffer.BlockCopy(buffer, offset, salt, 0, 32);
-            offset += 32;
-
-            Buffer.BlockCopy(buffer, offset, hash, 0, 32);
-            offset += 32;
-
-            Buffer.BlockCopy(buffer, offset, encrypted, 0, encrypted.Length);
+            return Load(byteStream, 100000);
         }
     }
 }
